69a8221e8d48f9ddae850a0ee9ba9e1df4880ead
Jenny Cruz <jennifer.ylee01@gmail.com>
2026-06-11 22:16:17 -0700

    linux babyyyyyy

---
+++ home/environment/fcitx5/default.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+{
+  il8n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fctix5-mozc
+        fctix5-hangul
+        fcitx5-chewing
+
+        fctix5-gtk
+        kdePackages.fcitx5-qt
+        qt6Packages.fcitx5-configtool
+      ];
+      settings = {
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us"
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "mozc";
+          "Groups/0/Items/2".Name = "hangul";
+          "Groups/0/Items/3".Name = "chewing";
+        };
+      };
+      waylandFrontend = true;
+    };
+  };
+}
---
+++ home/environment/kde/default.nix
@@ -0,0 +1,51 @@
+{
+  inputs,
+  pkgs,
+  config,
+  username,
+  ...
+}:
+{
+  imports = [
+    inputs.plasma-manager.homeModules.plasma-manager
+  ];
+
+  programs.plasma = {
+    enable = true;
+    workspae = {
+      lookAndFeel = "org.kde.breeze.desktop";
+      theme = "breeze-dark";
+      colorScheme = "BreezeDark";
+    };
+    configFile.kwinrc.Wayland.InputMethod.value =
+      "/etc/profiles/per-user/${username}/share/applications/org.fcitx.Fcitx5.desktop";
+    configFile.kde5rc = {
+      "module-gtkconfig"."autoload" = false;
+    };
+  };
+
+  gtk = {
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "kde";
+    style.name = "breeze";
+  };
+
+  home.packages = with pkgs; [
+    kdePackages.breeze-icons
+    kdePackages.kde-gtk-config
+  ];
+
+  home.sessionVariables = {
+    QT_QPA_PLATFORMTHEME = "kde";
+  };
+}
